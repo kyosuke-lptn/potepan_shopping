8ea0341e3b84523c076887c23bd6ee1710981fc5
kyosuke-lptn <sspvt065@gmail.com>
2019-08-25 08:31:22 +0000

    modify by rubocop

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,7 +2,8 @@ Spree::ProductsController.class_eval do
   before_action :product_images, only: :show
 
   private
-    def product_images
-      @product_images = (@product.images + @product.variant_images).uniq
-    end
+
+  def product_images
+    @product_images = (@product.images + @product.variant_images).uniq
+  end
 end
