7b543727025e2c1e8b98d7cba8d01415be32f500
kyosuke-lptn <sspvt065@gmail.com>
2019-08-28 14:58:17 +0000

    modifay view(header,spree/show,sample/index), rspec(delete tests related to taxons)

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -11,19 +11,8 @@ FactoryBot.define do
     deleted_at { nil }
     shipping_category { |r| Spree::ShippingCategory.first || r.association(:shipping_category) }
 
-    trait :with_taxons_and_images do
+    trait :with_images do
       after :create do |product_show|
-        taxonomy = create(:taxonomy)
-        root_taxon = taxonomy.root
-        parent_taxon = create(:taxon,
-                              name: 'Parent',
-                              taxonomy_id: taxonomy.id,
-                              parent: root_taxon)
-        child_taxon = create(:taxon,
-                             name: 'Parent',
-                             taxonomy_id: taxonomy.id,
-                             parent: parent_taxon)
-        [parent_taxon, child_taxon].each { |t| product_show.taxons << t }
         image = File.open(File.expand_path('../fixtures/thinking-cat.jpg', __dir__))
         product_show.images.create!(attachment: image)
       end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe 'Products機能', type: :system do
-  let!(:product) { create(:product_show, :with_taxons_and_images, name: 'sample1') }
-  let!(:product2) { create(:product_show, :with_taxons_and_images, name: 'sample2') }
+  let!(:product) { create(:product_show, :with_images, name: 'sample1') }
 
   describe '詳細表示機能' do
     before do
-      visit "potepan/products/#{product.slug}"
+      visit product_path(product)
     end
 
     context '商品詳細情報の表示' do
@@ -23,10 +22,6 @@ describe 'Products機能', type: :system do
       it '商品の小さい画像' do
         img = first(".thumb").find("img")
         expect(img[:alt]).to eq 'product-thumb-img'
-      end
-      it '関連商品へのリンク' do
-        img = first(".productImage").find("img")
-        expect(img[:alt]).to eq "products-img"
       end
     end
   end
