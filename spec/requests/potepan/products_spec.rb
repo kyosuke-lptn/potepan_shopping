65b9849daf86928ce835b3e28f03f6daf962f1d3
sumitani <sspvt065@gmail.com>
2019-09-04 18:06:05 +0000

    modify rspec(product)

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe "Products", type: :request do
 
   it "responds seuccessfully" do
     get potepan_product_path(product)
-    aggregate_failures do
-      expect(response).to be_successful
-      expect(response).to have_http_status "200"
-    end
+    expect(response).to be_successful
+    expect(response).to have_http_status "200"
   end
 end
