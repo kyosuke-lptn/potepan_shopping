9c4f48065885376223b4f4180dc97acb2f6bb9ff
Sumitani <sspvt065@example.com>
2019-09-11 13:55:17 +0000

    modify view(taxon#show) and add request-spec(product, taxon)

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
-  let!(:product) { create(:product) }
-
   describe "GET /potepan/products/:id" do
-    it "responds seuccessfully" do
-      get potepan_product_path(product)
-      expect(response).to be_successful
+    context "productが存在する時" do
+      let!(:product) { create(:product) }
+
+      it "responds seuccessfully" do
+        get potepan_product_path(product)
+        expect(response).to be_successful
+      end
+      it "product-nameが表示される" do
+        get potepan_product_path(product)
+        expect(response.body).to include product.name
+      end
+    end
+
+    context "productが存在しない時" do
+      it "raise an error" do
+        expect do
+          get potepan_product_path 1
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
--- spec/requests/potepan/taxons_spec.rb
+++ spec/requests/potepan/taxons_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Taxons", type: :request do
-  let(:taxon) { create(:taxon) }
-
   describe "GET /potepan/taxon_id" do
-    it "responds seuccessfully with taxon_id" do
-      get potepan_taxon_path(taxon.id)
-      expect(response).to have_http_status(200)
+    context "taxonが存在する時" do
+      let(:taxon) { create(:taxon) }
+
+      it "responds successfully with taxon_id" do
+        get potepan_taxon_path(taxon.id)
+        expect(response).to have_http_status(200)
+      end
+      it "taxon-nameが表示される" do
+        get potepan_taxon_path(taxon.id)
+        expect(response.body).to include taxon.name
+      end
+      it "doesn't respond successfully with taxon_permalink" do
+        expect do
+          get potepan_taxon_path(taxon)
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
-    it "doesn't respond seuccessfully with taxon_permalink" do
-      expect do
-        get potepan_taxon_path(taxon)
-      end.to raise_error(ActiveRecord::RecordNotFound)
+
+    context "taxonが存在しない時" do
+      it "raise an error" do
+        expect do
+          get potepan_taxon_path 1
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
