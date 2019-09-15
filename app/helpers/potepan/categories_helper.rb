dc8da4637c8d74ef293efa9ae013e6ed4f7c54b2
Kyosuke Sumitani <sspvt065@gmail.com>
2019-09-15 19:53:27 +0000

    Refactoring category_helper and modify view(category#show)

--- app/helpers/potepan/categories_helper.rb
+++ app/helpers/potepan/categories_helper.rb
@@ -1,64 +1,58 @@
 module Potepan
   module CategoriesHelper
     def taxons_list(taxonomies)
-      tag.ul class: "nav navbar-nav side-nav", id: "categories" do
+      tag.ul id: "categories", class: "nav navbar-nav side-nav" do
         taxonomies.each do |taxonomy|
           taxon_root =  taxonomy.root
-          concat(
-            tag.li do
-              concat(link_to("javascript:;", id: "category-parent", data: {
-                toggle: "collapse",
-                target: "\##{taxon_root.name}",
-              })do
-                concat taxon_root.name
-                concat tag.i class: "fa fa-plus"
-              end)
-              concat child_list(taxon_root)
+          list_connection(taxon_root, a_id: "category-parent") do
+            tag.ul id: taxon_root.name, class: "collapse collapseItem" do
+              taxon_root.children.each do |child|
+                if child.children.empty?
+                  concat lowest_list(child)
+                else
+                  list_connection(child, a_id: "category-child") do
+                    tag.ul(id: child.name, class: "collapse collapseItem") do
+                      child.children.each do |grandchild|
+                        concat lowest_list(grandchild, li_class: "grandchild")
+                      end
+                    end
+                  end
+                end
+              end
             end
-          )
+          end
         end
       end
     end
 
-    def child_list(taxon)
-      tag.ul id: taxon.name, class: "collapse collapseItem" do
-        taxon.children.each do |child|
-          concat(
-            tag.li do
-              if child.children.empty?
-                link_to potepan_category_path(child.id) do
-                  concat tag.i(class: "fa fa-angle-right", aria: { hidden: true })
-                  concat child.name
-                  concat tag.span "(#{child.all_products.count})"
-                end
-              else
-                concat(link_to("javascript:;", id: "category-child", data: {
-                  toggle: "collapse",
-                  target: "\##{child.name}",
-                })do
-                  concat child.name
-                  concat tag.i class: "fa fa-caret-right"
-                end)
-                concat(
-                  tag.ul(id: child.name, class: "collapse collapseItem")do
-                    child.children.each do |grandchild|
-                      concat(
-                        tag.li(class: "grandchild") do
-                          link_to potepan_category_path(grandchild.id) do
-                            concat tag.i(class: "fa fa-angle-right", aria: { hidden: true })
-                            concat grandchild.name
-                            concat tag.span "(#{grandchild.active_products.count})"
-                          end
-                        end
-                      )
-                    end
-                  end
-                )
-              end
-            end
-          )
+    def lowest_list(taxon, li_class: nil)
+      tag.li(class: "#{li_class}") do
+        link_to potepan_category_path(taxon.id) do
+          concat tag.i(class: "fa fa-angle-right", aria: { hidden: true })
+          concat taxon.name
+          concat tag.span "(#{taxon.active_products.count})"
         end
       end
+    end
+
+    def list_connection(taxon, a_id: nil)
+      icon_class = (a_id == "category-parent") ? "fa fa-plus" : "fa fa-caret-right"
+      concat(
+        tag.li do
+          concat(
+            link_to("javascript:;", id: a_id, data: {
+              toggle: "collapse",
+              target: "\##{taxon.name}",
+            }) do
+              concat taxon.name
+              concat tag.i class: icon_class
+            end
+          )
+          concat(
+            yield
+          )
+        end
+      )
     end
   end
 end
