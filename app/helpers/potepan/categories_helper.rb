e8fcafb26806e43c5b896aaff4e347664d91690c
Kyosuke Sumitani <sspvt065@gmail.com>
2019-09-15 17:38:21 +0000

    modify categories_helper

--- app/helpers/potepan/categories_helper.rb
+++ app/helpers/potepan/categories_helper.rb
@@ -6,14 +6,14 @@ module Potepan
           taxon_root =  taxonomy.root
           concat(
             tag.li do
-              link_to "javascript:;", data: {
+              concat(link_to("javascript:;", id: "category-parent", data: {
                 toggle: "collapse",
                 target: "\##{taxon_root.name}",
-              } do
+              })do
                 concat taxon_root.name
                 concat tag.i class: "fa fa-plus"
-                concat child_list(taxon_root)
-              end
+              end)
+              concat child_list(taxon_root)
             end
           )
         end
@@ -25,10 +25,35 @@ module Potepan
         taxon.children.each do |child|
           concat(
             tag.li do
-              link_to potepan_category_path(child.id) do
-                concat tag.i(class: "fa fa-caret-right", aria: { hidden: true })
-                concat child.name
-                concat tag.span "(#{child.all_products.count})"
+              if child.children.empty?
+                link_to potepan_category_path(child.id) do
+                  concat tag.i(class: "fa fa-angle-right", aria: { hidden: true })
+                  concat child.name
+                  concat tag.span "(#{child.all_products.count})"
+                end
+              else
+                concat(link_to("javascript:;", id: "category-child", data: {
+                  toggle: "collapse",
+                  target: "\##{child.name}",
+                })do
+                  concat child.name
+                  concat tag.i class: "fa fa-caret-right"
+                end)
+                concat(
+                  tag.ul(id: child.name, class: "collapse collapseItem")do
+                    child.children.each do |grandchild|
+                      concat(
+                        tag.li(class: "grandchild") do
+                          link_to potepan_category_path(grandchild.id) do
+                            concat tag.i(class: "fa fa-angle-right", aria: { hidden: true })
+                            concat grandchild.name
+                            concat tag.span "(#{grandchild.active_products.count})"
+                          end
+                        end
+                      )
+                    end
+                  end
+                )
               end
             end
           )
--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -4,19 +4,24 @@ describe 'Categories機能', type: :system do
   let(:product) { create(:product) }
   let!(:taxonomy) { create(:taxonomy, name: 'Categories') }
   let(:taxon) { create(:taxon, parent_id: taxonomy.root.id, taxonomy: taxonomy) }
+  let(:taxon_child) { create(:taxon, parent_id: taxon.id, taxonomy: taxonomy) }
 
   before do
-    product.taxons << taxon
+    product.taxons << taxon_child
   end
 
   it "カテゴリー関連ページの確認" do
-    visit potepan_category_path(taxon.id)
+    visit potepan_category_path(taxon_child.id)
 
     aggregate_failures do
       expect(page).to have_title "#{taxon.name} - BIGBAG Store"
       within '#categories' do
         expect(page).to have_content taxon.root.name
-        expect(page).to have_link taxon.name
+        expect(page).to have_content taxon.name
+        expect(page).to have_link taxon_child.name
+        expect(page).to have_css '#category-parent'
+        expect(page).to have_css '#category-child'
+        expect(page).to have_css '.grandchild'
       end
       expect(page).to have_link product.name
       expect(page).to have_content product.display_price
