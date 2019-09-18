b5d191b303a08f702b5ce4dfdb5c3e2e2c9a5b24
Kyosuke Sumitani <sspvt065@gmail.com>
2019-09-18 16:19:53 +0000

    fix for products controller and add link to products#show

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -5,16 +5,16 @@ class Potepan::ProductsController < Spree::ProductsController
 
   def load_variables
     @product_images = (@product.images + @product.variant_images).uniq || []
-    taxons = @product.taxons
-    if taxons.empty?
+    @taxons = @product.taxons
+    if @taxons.empty?
       @taxon_products = []
     else
-      product_ids = taxons.map do |taxon|
+      product_ids = @taxons.map do |taxon|
         taxon.products.where.not(id: @product.id).ids
       end.flatten!.uniq
       @taxon_products = Spree::Product.
         includes(master: [:images, :currently_valid_prices]).
-        where(id: product_ids)
+        where(id: product_ids[0..3])
     end
   end
 end
