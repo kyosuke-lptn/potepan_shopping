7f48b80cd5b4b9e5c8791a723cb40f3ba3bac90e
kyosuke <sspvt065@gmail.com>
2019-10-25 21:36:50 +0900

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -104,6 +104,6 @@ Rails.application.configure do
     bucket: 'potepanec20191023'
   }
 
-  config.default_url_options = { host: "potepan-shopping.herokuapp.com" }
+  Spree::Core::Engine.routes.default_url_options = { host: "potepan-shopping.herokuapp.com" }
   config.action_mailer.default_url_options = { host: "potepan-shopping.herokuapp.com" }
 end
